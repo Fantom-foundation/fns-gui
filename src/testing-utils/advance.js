547eb642f9f02e22e8210158b4e024b806b519ca
hashrider <alexanderyy07@gmail.com>
2021-10-20 19:01:05 -0600

    Installed new apollo client

--- src/testing-utils/advance.js
+++ src/testing-utils/advance.js
@@ -1,17 +1,17 @@
-const Web3 = require('web3')
-const util = require('util')
-const DAYS = 24 * 60 * 60
+const Web3 = require('web3');
+const util = require('util');
+const DAYS = 24 * 60 * 60;
 
-let web3
+let web3;
 
 async function setupWeb3(customProvider) {
-  web3 = new Web3(customProvider)
-  const networkId = await web3.eth.net.getId()
+  web3 = new Web3(customProvider);
+  const networkId = await web3.eth.net.getId();
 
   return {
     web3,
     networkId
-  }
+  };
 }
 
 const advanceTime = util.promisify(function(web3, delay, done) {
@@ -22,8 +22,8 @@ const advanceTime = util.promisify(function(web3, delay, done) {
       params: [delay]
     },
     done
-  )
-})
+  );
+});
 
 const mine = util.promisify(function(web3, done) {
   return web3.currentProvider.send(
@@ -32,19 +32,19 @@ const mine = util.promisify(function(web3, done) {
       method: 'evm_mine'
     },
     done
-  )
-})
+  );
+});
 
 async function init() {
-  let provider = new Web3.providers.HttpProvider('http://localhost:8545')
-  let { web3 } = await setupWeb3(provider)
+  let provider = new Web3.providers.HttpProvider('http://localhost:8545');
+  let { web3 } = await setupWeb3(provider);
   let current = await web3.eth.getBlock('latest');
-  console.log(`The current time is ${new Date(current.timestamp * 1000)}`)
+  console.log(`The current time is ${new Date(current.timestamp * 1000)}`);
 
-  await advanceTime(web3, DAYS * 365)
-  await mine(web3)
+  await advanceTime(web3, DAYS * 365);
+  await mine(web3);
   current = await web3.eth.getBlock('latest');
-  console.log(`The current time is ${new Date(current.timestamp * 1000)}`)
+  console.log(`The current time is ${new Date(current.timestamp * 1000)}`);
 }
 
-init()
+init();
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,4 @@
-import { getNetworkId } from '@ensdomains/ui/src/web3'
+import { getNetworkId } from '@ensdomains/ui/src/web3';
 import {
   emptyAddress as _emptyAddress,
   validateName as _validateName,
@@ -6,165 +6,168 @@ import {
   getEnsStartBlock as _ensStartBlock,
   isLabelValid as _isLabelValid,
   isEncodedLabelhash
-} from '@ensdomains/ui/src/utils/index'
-import { validate } from '@ensdomains/ens-validation'
+} from '@ensdomains/ui/src/utils/index';
+import { validate } from '@ensdomains/ens-validation';
 
-import getENS from '../api/ens'
-import * as jsSHA3 from 'js-sha3'
-import { saveName } from '../api/labels'
-import { setup } from '../api/ens'
-import { SET_ERROR } from 'graphql/mutations'
-import { setupClient } from 'apolloClient'
-import { connect } from '../api/web3modal'
-import { safeInfo, setupSafeApp } from './safeApps'
-import { useEffect, useRef } from 'react'
-import { normalize } from 'eth-ens-namehash'
+import getENS from '../api/ens';
+import * as jsSHA3 from 'js-sha3';
+import { saveName } from '../api/labels';
+import { setup } from '../api/ens';
+import { SET_ERROR } from 'graphql/mutations';
+import { setupClient } from 'apolloClient';
+import { connect } from '../api/web3modal';
+import { safeInfo, setupSafeApp } from './safeApps';
+import { useEffect, useRef } from 'react';
+import { normalize } from 'eth-ens-namehash';
 
 // From https://github.com/0xProject/0x-monorepo/blob/development/packages/utils/src/address_utils.ts
 
-const BASIC_ADDRESS_REGEX = /^(0x)?[0-9a-f]{40}$/i
-const SAME_CASE_ADDRESS_REGEX = /^(0x)?([0-9a-f]{40}|[0-9A-F]{40})$/
-const ADDRESS_LENGTH = 40
+const BASIC_ADDRESS_REGEX = /^(0x)?[0-9a-f]{40}$/i;
+const SAME_CASE_ADDRESS_REGEX = /^(0x)?([0-9a-f]{40}|[0-9A-F]{40})$/;
+const ADDRESS_LENGTH = 40;
 export const MAINNET_DNSREGISTRAR_ADDRESS =
-  '0x58774Bb8acD458A640aF0B88238369A167546ef2'
+  '0x58774Bb8acD458A640aF0B88238369A167546ef2';
 export const ROPSTEN_DNSREGISTRAR_ADDRESS =
-  '0xdB328BA5FEcb432AF325Ca59E3778441eF5aa14F'
+  '0xdB328BA5FEcb432AF325Ca59E3778441eF5aa14F';
 
 export const addressUtils = {
   isChecksumAddress(address) {
     // Check each case
-    const unprefixedAddress = address.replace('0x', '')
-    const addressHash = jsSHA3.keccak256(unprefixedAddress.toLowerCase())
+    const unprefixedAddress = address.replace('0x', '');
+    const addressHash = jsSHA3.keccak256(unprefixedAddress.toLowerCase());
 
     for (let i = 0; i < ADDRESS_LENGTH; i++) {
       // The nth letter should be uppercase if the nth digit of casemap is 1
-      const hexBase = 16
-      const lowercaseRange = 7
+      const hexBase = 16;
+      const lowercaseRange = 7;
       if (
         (parseInt(addressHash[i], hexBase) > lowercaseRange &&
           unprefixedAddress[i].toUpperCase() !== unprefixedAddress[i]) ||
         (parseInt(addressHash[i], hexBase) <= lowercaseRange &&
           unprefixedAddress[i].toLowerCase() !== unprefixedAddress[i])
       ) {
-        return false
+        return false;
       }
     }
-    return true
+    return true;
   },
   isAddress(address) {
     if (!BASIC_ADDRESS_REGEX.test(address)) {
       // Check if it has the basic requirements of an address
-      return false
+      return false;
     } else if (SAME_CASE_ADDRESS_REGEX.test(address)) {
       // If it's all small caps or all all caps, return true
-      return true
+      return true;
     } else {
       // Otherwise check each case
-      const isValidChecksummedAddress = addressUtils.isChecksumAddress(address)
-      return isValidChecksummedAddress
+      const isValidChecksummedAddress = addressUtils.isChecksumAddress(address);
+      return isValidChecksummedAddress;
     }
   }
-}
+};
 
 export const uniq = (a, param) =>
   a.filter(
     (item, pos) => a.map(mapItem => mapItem[param]).indexOf(item[param]) === pos
-  )
+  );
 
 export async function getEtherScanAddr() {
-  const networkId = await getNetworkId()
+  const networkId = await getNetworkId();
   switch (networkId) {
     case 1:
     case '1':
-      return 'https://etherscan.io/'
+      return 'https://etherscan.io/';
     case 3:
     case '3':
-      return 'https://ropsten.etherscan.io/'
+      return 'https://ropsten.etherscan.io/';
     case 4:
     case '4':
-      return 'https://rinkeby.etherscan.io/'
+      return 'https://rinkeby.etherscan.io/';
     default:
-      return 'https://etherscan.io/'
+      return 'https://etherscan.io/';
   }
 }
 
 export async function ensStartBlock() {
-  return _ensStartBlock()
+  return _ensStartBlock();
 }
 
-export const checkLabels = (...labelHashes) => labelHashes.map(hash => null)
+export const checkLabels = (...labelHashes) => labelHashes.map(hash => null);
 
 // export const checkLabels = (...labelHashes) =>
 //   labelHashes.map(labelHash => checkLabelHash(labelHash) || null)
 
 export const mergeLabels = (labels1, labels2) =>
-  labels1.map((label, index) => (label ? label : labels2[index]))
+  labels1.map((label, index) => (label ? label : labels2[index]));
 
 export function validateName(name) {
-  const normalisedName = _validateName(name)
-  saveName(normalisedName)
-  return normalisedName
+  const normalisedName = _validateName(name);
+  saveName(normalisedName);
+  return normalisedName;
 }
 
 export function isLabelValid(name) {
-  return _isLabelValid(name)
+  return _isLabelValid(name);
 }
 
 export const parseSearchTerm = async term => {
-  const ens = getENS()
-  const domains = term.split('.')
-  const tld = domains[domains.length - 1]
+  const ens = getENS();
+  const domains = term.split('.');
+  const tld = domains[domains.length - 1];
   try {
-    _validateName(tld)
+    _validateName(tld);
   } catch (e) {
-    return 'invalid'
+    return 'invalid';
   }
-  console.log('** parseSearchTerm', { ens })
-  const address = await ens.getOwner(tld)
-  return _parseSearchTerm(term, true)
-}
+  console.log('** parseSearchTerm', { ens });
+  const address = await ens.getOwner(tld);
+  return _parseSearchTerm(term, true);
+};
 
 export function humaniseName(name) {
   return name
     .split('.')
     .map(label => {
-      return isEncodedLabelhash(label) ? `[unknown${label.slice(1, 8)}]` : label
+      return isEncodedLabelhash(label)
+        ? `[unknown${label.slice(1, 8)}]`
+        : label;
     })
-    .join('.')
+    .join('.');
 }
 
 export function modulate(value, rangeA, rangeB, limit) {
-  let fromHigh, fromLow, result, toHigh, toLow
+  let fromHigh, fromLow, result, toHigh, toLow;
   if (limit === null) {
-    limit = false
+    limit = false;
   }
-  fromLow = rangeA[0]
-  fromHigh = rangeA[1]
-  toLow = rangeB[0]
-  toHigh = rangeB[1]
-  result = toLow + ((value - fromLow) / (fromHigh - fromLow)) * (toHigh - toLow)
+  fromLow = rangeA[0];
+  fromHigh = rangeA[1];
+  toLow = rangeB[0];
+  toHigh = rangeB[1];
+  result =
+    toLow + ((value - fromLow) / (fromHigh - fromLow)) * (toHigh - toLow);
   if (limit === true) {
     if (toLow < toHigh) {
       if (result < toLow) {
-        return toLow
+        return toLow;
       }
       if (result > toHigh) {
-        return toHigh
+        return toHigh;
       }
     } else {
       if (result > toLow) {
-        return toLow
+        return toLow;
       }
       if (result < toHigh) {
-        return toHigh
+        return toHigh;
       }
     }
   }
-  return result
+  return result;
 }
 
 export function isElementInViewport(el) {
-  var rect = el.getBoundingClientRect()
+  var rect = el.getBoundingClientRect();
 
   return (
     rect.top >= 0 &&
@@ -175,27 +178,27 @@ export function isElementInViewport(el) {
     rect.right <=
       (window.innerWidth ||
         document.documentElement.clientWidth) /*or $(window).width() */
-  )
+  );
 }
 
-export const emptyAddress = _emptyAddress
+export const emptyAddress = _emptyAddress;
 
 export function isShortName(term) {
-  return [...term].length < 3
+  return [...term].length < 3;
 }
 
 export const aboutPageURL = () => {
-  const lang = window.localStorage.getItem('language') || ''
+  const lang = window.localStorage.getItem('language') || '';
 
-  return `https://ens.domains/${lang === 'en' ? '' : lang}`
-}
+  return `https://ens.domains/${lang === 'en' ? '' : lang}`;
+};
 
 export function isRecordEmpty(value) {
-  return value === emptyAddress || value === ''
+  return value === emptyAddress || value === '';
 }
 
 export async function handleNetworkChange() {
-  let client, networkId
+  let client, networkId;
   try {
     if (
       process.env.REACT_APP_STAGE === 'local' &&
@@ -205,88 +208,88 @@ export async function handleNetworkChange() {
         reloadOnAccountsChange: true,
         customProvider: 'http://localhost:8545',
         ensAddress: process.env.REACT_APP_ENS_ADDRESS
-      })
+      });
       let labels = window.localStorage['labels']
         ? JSON.parse(window.localStorage['labels'])
-        : {}
+        : {};
       window.localStorage.setItem(
         'labels',
         JSON.stringify({
           ...labels,
           ...JSON.parse(process.env.REACT_APP_LABELS)
         })
-      )
+      );
     } else {
-      const safe = await safeInfo()
+      const safe = await safeInfo();
       if (safe) {
-        const network = await setupSafeApp(safe)
+        const network = await setupSafeApp(safe);
       } else if (window.localStorage.getItem('WEB3_CONNECT_CACHED_PROVIDER')) {
-        const network = await connect()
+        const network = await connect();
       } else {
         await setup({
           reloadOnAccountsChange: false,
           enforceReadOnly: true,
           enforceReload: true
-        })
+        });
       }
     }
-    networkId = await getNetworkId()
-    client = await setupClient(networkId)
+    networkId = await getNetworkId();
+    client = await setupClient(networkId);
   } catch (e) {
-    networkId = networkId || 1 // Readonly to Mainnet
-    client = await setupClient()
+    networkId = networkId || 1; // Readonly to Mainnet
+    client = await setupClient();
     await client.mutate({
       mutation: SET_ERROR,
       variables: { message: e && e.message }
-    })
+    });
   }
-  return { client, networkId }
+  return { client, networkId };
 }
 
 export const hasValidReverseRecord = getReverseRecord =>
-  getReverseRecord?.name && getReverseRecord.name !== emptyAddress
+  getReverseRecord?.name && getReverseRecord.name !== emptyAddress;
 
 export const hasNonAscii = () => {
-  const strs = window.location.pathname.split('/')
+  const strs = window.location.pathname.split('/');
   const rslt = strs.reduce((accum, next) => {
-    if (accum) return true
-    if (!validate(next)) return true
-    return accum
-  }, false)
-  return rslt
-}
+    if (accum) return true;
+    if (!validate(next)) return true;
+    return accum;
+  }, false);
+  return rslt;
+};
 
 export function usePrevious(value) {
   // The ref object is a generic container whose current property is mutable ...
   // ... and can hold any value, similar to an instance property on a class
-  const ref = useRef()
+  const ref = useRef();
   // Store current value in ref
   useEffect(() => {
-    ref.current = value
-  }, [value]) // Only re-run if value changes
+    ref.current = value;
+  }, [value]); // Only re-run if value changes
   // Return previous value (happens before update in useEffect above)
-  return ref.current
+  return ref.current;
 }
 
 export function filterNormalised(data, name, nested = false) {
   return data.filter(data => {
-    const domain = nested ? data.domain : data
-    return domain[name] === normalize(domain[name])
-  })
+    const domain = nested ? data.domain : data;
+    return domain[name] === normalize(domain[name]);
+  });
 }
 
 export function prependUrl(url) {
   if (url && !url.match(/http[s]?:\/\//)) {
-    return 'https://' + url
+    return 'https://' + url;
   } else {
-    return url
+    return url;
   }
 }
 
 export function imageUrl(url, name, network) {
   if (name && network === 'rinkeby' && url.match(/^eip/)) {
-    return `https://ens-metadata-service.appspot.com/avatar/${name}`
+    return `https://ens-metadata-service.appspot.com/avatar/${name}`;
   } else {
-    return prependUrl(url)
+    return prependUrl(url);
   }
 }
