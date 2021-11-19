2ccee81437e0805b21f238178809a9edd7b8b0a4
hashrider <alexanderyy07@gmail.com>
2021-11-19 16:25:18 -0700

    Fixed toggle function between registrant, controller

--- src/components/Address/Filtering.js
+++ src/components/Address/Filtering.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import { useHistory } from 'react-router'
-import { useTranslation } from 'react-i18next'
+import React from 'react';
+import { useHistory } from 'react-router';
+import { useTranslation } from 'react-i18next';
 
-import { Tab, Tabs } from '../Tabs'
+import { Tab, Tabs } from '../Tabs';
 
 function getBaseUrl(url) {
-  const urlArray = url.split('/')
-  if (urlArray.length === 3) return url
-  return urlArray.slice(0, -1).join('/')
+  const urlArray = url.split('/');
+  if (urlArray.length === 3) return url;
+  return urlArray.slice(0, -1).join('/');
 }
 
 export default function Filtering({
@@ -17,26 +17,32 @@ export default function Filtering({
   className,
   url
 }) {
-  let { t } = useTranslation()
-  const history = useHistory()
-  const baseUrl = getBaseUrl(url)
+  let { t } = useTranslation();
+  const history = useHistory();
+  const baseUrl = getBaseUrl(url);
   return (
     <Tabs className={className}>
       <Tab
         active={activeFilter === 'registrant'}
-        onClick={() => history.push(`${baseUrl}/registrant`)}
+        onClick={() => {
+          setActiveSort({
+            type: 'expiryDate',
+            direction: 'asc'
+          });
+          history.push(`${baseUrl}/registrant`);
+        }}
       >
         {t('address.filter.registrant')}
       </Tab>
       <Tab
         active={activeFilter === 'controller'}
         onClick={() => {
-          history.push(`${baseUrl}/controller`)
-          setActiveSort('alphabetical')
+          history.push(`${baseUrl}/controller`);
+          setActiveSort('alphabetical');
         }}
       >
         {t('address.filter.controller')}
       </Tab>
     </Tabs>
-  )
+  );
 }
