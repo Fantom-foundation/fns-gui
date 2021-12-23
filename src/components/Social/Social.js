4914cf85d1621c2556192fe5f2f54fe9ca075c97
hashrider <alexanderyy07@gmail.com>
2021-12-23 14:05:09 -0700

    Added new social links

--- src/components/Social/Social.js
+++ src/components/Social/Social.js
@@ -3,7 +3,8 @@ import styled from '@emotion/styled/macro';
 
 import github from '../Icons/Github.svg';
 import twitter from '../Icons/Twitter.svg';
-import medium from '../Icons/Medium.svg';
+import discord from '../Icons/Discord.svg';
+import telegram from '../Icons/Telegram.svg';
 
 import mq from 'mediaQuery';
 import { withRouter } from 'react-router-dom';
@@ -64,58 +65,18 @@ const ExternalLink = styled('a')`
       fill: #1969ff;
     }
   }
-`;
 
-const ThirdPartyLink = styled('a')`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-weight: 200;
-  font-size: 22px;
-  color: ${p => (p.active ? '#1969ff' : '#B1BBCE')};
-  padding: 20px 0;
-  border-bottom: 1px solid rgba(255, 255, 255, 0.2);
-
-  ${mq.medium`
-    justify-content: start;
-    border-bottom: 0;
-  `}
-
-  &:visited {
-    color: #1969ff;
-  }
-
-  span {
-    transition: 0.2s;
-    margin-left: 15px;
-    font-family: Overpass;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 16px;
-    line-height: 1;
-    padding-top: 2px;
-    letter-spacing: -0.5px;
-    color: ${p => (p.active ? '#161B24' : '#B1BBCE')};
-  }
-
-  &:hover {
-    span {
-      color: #1969ff;
-    }
-    path {
-      fill: #1969ff;
-    }
-    g {
-      fill: #1969ff;
-    }
+  img {
+    width: 24px;
+    height: 24px;
   }
 `;
 
 const social = [
   {
-    img: github,
-    text: 'Github',
-    link: 'https://github.com/Fantom-Foundation'
+    img: discord,
+    text: 'Discord',
+    link: 'http://chat.fantom.network/'
   },
   {
     img: twitter,
@@ -123,9 +84,14 @@ const social = [
     link: 'https://twitter.com/FantomFDN'
   },
   {
-    img: medium,
-    text: 'Medium',
-    link: 'https://medium.com/fantomfoundation'
+    img: telegram,
+    text: 'Telegram',
+    link: 'https://t.me/Fantom_English'
+  },
+  {
+    img: github,
+    text: 'Github',
+    link: 'https://github.com/Fantom-Foundation'
   }
 ];
 
