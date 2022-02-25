3392d39a1254011208edb8fad7b2706843b330cf
Alexander <alexanderyy07@gmail.com>
2022-02-24 20:16:55 -0700

    Added price input and toast for auction page

---
+++ src/utils/toast/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import toast from 'react-hot-toast';
+
+import iconInfo from 'assets/svgs/info.svg';
+import iconSuccess from 'assets/svgs/success.svg';
+import iconError from 'assets/svgs/error.svg';
+import iconWarning from 'assets/svgs/warning.svg';
+
+import styles from './styles.module.scss';
+
+const icons = {
+  info: iconInfo,
+  success: iconSuccess,
+  error: iconError,
+  warning: iconWarning
+};
+
+export default (type, title, body = '', onClick = () => {}) => {
+  return toast(
+    () => (
+      <div className={styles.toastInner} onClick={onClick}>
+        <div className={styles.header}>
+          <img src={icons[type]} alt={type} className={styles.icon} />
+          <span>{title}</span>
+        </div>
+        {body.length > 0 && <div className={styles.body}>{body}</div>}
+      </div>
+    ),
+    {
+      duration: 5000,
+      className: styles.toast
+    }
+  );
+};
